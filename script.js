69cf21bde1878d412b685965b37d6c06281a1b78
Sarvadnya <sarvadnya.mistry18@gmail.com>
2025-01-25 20:22:49 +0530

    deploy

---
+++ script.js
@@ -0,0 +1,59 @@
+//     const hamburger = document.querySelector('.hamburger');
+//     const navLinks = document.querySelector('.nav-links');
+
+//     hamburger.addEventListener('click', () => {
+//         navLinks.classList.toggle('active');
+//         hamburger.classList.toggle('active');
+//     });
+
+//     var navBar = document.getElementById("navbar");
+//       window.onscroll = function() {
+//         if (window.scrollY > 22) {
+//           navBar.classList.add("scrolled");
+//         } else {
+//           navBar.classList.remove("scrolled");
+//         }
+//       };
+
+
+
+//     //   const scrollToTopButton = document.getElementById('scrollToTop');
+  
+//     //   // Show or hide the button based on scroll position
+//     //   window.addEventListener('scroll', () => {
+//     //       if (window.scrollY > 300) {
+//     //           scrollToTopButton.classList.remove('hidden');
+//     //       } else {
+//     //           scrollToTopButton.classList.add('hidden');
+//     //       }
+//     //   });
+  
+//     //   // Scroll to the top of the page when the button is clicked
+//     //   scrollToTopButton.addEventListener('click', () => {
+//     //       window.scrollTo({
+//     //           top: 0,
+//     //           behavior: 'smooth'
+//     //       });
+//     //   });
+  
+
+//     //toggle Icon Nav Bar 
+//     let menuIcon = document.querySelector('#menu-icon');
+//   let navbar = document.querySelector('.navbar');
+
+//   menuIcon.onclick = () => {
+//   menuIcon.classList.toggle('bx-x'); //box icons 
+//   navbar.classList.toggle('active');
+// };
+
+
+
+// let menuIcon = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
+
+// menuIcon.onclick = () => {
+//   menuIcon.classList.toggle('bx-x'); // Toggles the close icon
+//   navbar.classList.toggle('active'); // Toggles the visibility of the navbar
+// };
+
+
